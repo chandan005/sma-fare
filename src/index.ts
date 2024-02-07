a61a83b4fdf9e05e0948c3b4dacbad020fcff024
chandansingh005 <chandan.singh@brauz.com>
2024-02-08 03:09:59 +0545

    Removed comments.

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,6 @@ import * as fs from 'fs';
 import { parseCSV } from './services/CsvParser';
 import { calculateTotalFare } from './services/FareCalculator';
 
-// const program = new Command();
-// program
-//   .description('SMS Fare Command Line Application.')
-//   .arguments('<file>')
-//   .action(async (file) => {
-//     if (!fs.existsSync(file)) {
-//       console.error('Error: The specified file does not exist.');
-//       process.exit(1);
-//     }
-//     const journeys = await parseCSV(file);
-//     console.log(journeys);
-//     if (journeys && journeys.length > 0) {
-//       const totalFare = calculateTotalFare(journeys);
-//       console.log(`Total fare: ${totalFare}`);
-//     } else {
-//       console.log('No journeys were parsed from the csv file.');
-//     }
-//   })
-//   .parse(process.argv);
-
-// program.parse(process.argv);
-
 async function run() {
   figlet('Singa Metro Authority', (err, data) => {
     if (err) {
